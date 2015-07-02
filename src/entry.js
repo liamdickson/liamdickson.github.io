53ef2853adf2113ee68d6905270e04bb55ddcd00
Liam Dickson <liamsdickson@gmail.com>
2015-07-02 00:10:10 -0400

    removed router, probably not needed

--- src/entry.js
+++ src/entry.js
@@ -1,15 +1,9 @@
 'use strict';
 
-var Router = require('./Router');
 var React = require('react');
 var app = require('./app');
 
-var mRouter;
-
 var context = window.context = app.createContext();
-
-mRouter = new Router({context: context.getComponentContext()});
-mRouter.history.start({pushState: true});
 
 React.render(
 	context.createElement(),
